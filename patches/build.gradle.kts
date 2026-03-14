4ba9357747eb71420c37abc47f3fbeab1a6a3aee
LuisCupul04 <256816875+LuisCupul04@users.noreply.github.com>
2026-03-13 21:20:44 -0600

    Test-27-bug

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,29 +1,11 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    //kotlin("jvm")           // <-- Sin versión
+    kotlin("jvm")           // <-- Sin versión
     id("maven-publish")
 }
 
 group = "com.extenre"
-
-repositories {
-    mavenLocal()            // Para encontrar shared publicado localmente
-    mavenCentral()
-    google()
-    maven {
-        name = "GitHubPackages"
-        url = uri("https://maven.pkg.github.com/LuisCupul04/Extenre-patcher")
-        credentials {
-            username = System.getenv("GITHUB_ACTOR")
-            password = System.getenv("GITHUB_TOKEN")
-        }
-    }
-    maven {
-        name = "JitPack"
-        url = uri("https://jitpack.io")
-    }
-}
 
 // Configuración de la extensión 'patches' (asumo que existe un plugin o extensión)
 patches {
