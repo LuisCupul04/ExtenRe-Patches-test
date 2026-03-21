60edac2190b1ef9b2a64be3051e8d4f17614dcfc
LuisCupul04 <256816875+LuisCupul04@users.noreply.github.com>
2026-03-20 21:38:23 -0600

    Test-110

--- patches/src/main/kotlin/com/extenre/patches/youtube/misc/backgroundplayback/BackgroundPlaybackPatch.kt
+++ patches/src/main/kotlin/com/extenre/patches/youtube/misc/backgroundplayback/BackgroundPlaybackPatch.kt
@@ -9,33 +9,32 @@
 package com.extenre.patches.youtube.misc.backgroundplayback
 
 import com.extenre.patcher.extensions.InstructionExtensions.addInstruction
-import com.extenre.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
 import com.extenre.patcher.extensions.InstructionExtensions.getInstruction
-import com.extenre.patcher.patch.PatchException
+import com.extenre.patcher.extensions.InstructionExtensions.instructions
 import com.extenre.patcher.patch.bytecodePatch
-import com.extenre.patcher.util.smali.ExternalLabel
 import com.extenre.patches.youtube.utils.compatibility.Constants.COMPATIBLE_PACKAGE
-import com.extenre.patches.youtube.utils.extension.Constants.PLAYER_CLASS_DESCRIPTOR
+import com.extenre.patches.youtube.utils.extension.Constants.MISC_PATH
 import com.extenre.patches.youtube.utils.patch.PatchList.REMOVE_BACKGROUND_PLAYBACK_RESTRICTIONS
-import com.extenre.patches.youtube.utils.playservice.is_19_28_or_greater
+import com.extenre.patches.youtube.utils.playertype.playerTypeHookPatch
+import com.extenre.patches.youtube.utils.playservice.is_19_34_or_greater
 import com.extenre.patches.youtube.utils.playservice.versionCheckPatch
 import com.extenre.patches.youtube.utils.settings.ResourceUtils.addPreference
 import com.extenre.patches.youtube.utils.settings.settingsPatch
+import com.extenre.util.addInstructionsAtControlFlowLabel
+import com.extenre.util.findInstructionIndicesReversedOrThrow
+import com.extenre.util.fingerprint.injectLiteralInstructionBooleanCall
 import com.extenre.util.fingerprint.matchOrThrow
 import com.extenre.util.fingerprint.mutableMethodOrThrow
+import com.extenre.util.fingerprint.originalMethodOrThrow
 import com.extenre.util.getReference
-import com.extenre.util.getWalkerMethod
-import com.extenre.util.indexOfFirstInstruction
-import com.extenre.util.indexOfFirstInstructionOrThrow
-import com.extenre.util.indexOfFirstInstructionReversedOrThrow
-import com.extenre.util.indexOfFirstLiteralInstructionOrThrow
+import com.extenre.util.returnEarly
 import com.android.tools.smali.dexlib2.Opcode
-import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
-import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
-import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 import com.android.tools.smali.dexlib2.util.MethodUtil
+
+private const val EXTENSION_CLASS_DESCRIPTOR =
+    "$MISC_PATH/BackgroundPlaybackPatch;"
 
 @Suppress("unused")
 val backgroundPlaybackPatch = bytecodePatch(
@@ -45,94 +44,102 @@ val backgroundPlaybackPatch = bytecodePatch(
     compatibleWith(COMPATIBLE_PACKAGE)
 
     dependsOn(
+        playerTypeHookPatch,
         settingsPatch,
         versionCheckPatch,
     )
 
     execute {
-        // region patch for background playback
+        // region background playback for normal videos and shorts
 
-        backgroundPlaybackManagerFingerprint.mutableMethodOrThrow().addInstructions(
-            0, """
-                invoke-static {}, $PLAYER_CLASS_DESCRIPTOR->getBackgroundPlaybackState()Z
-                move-result v0
-                return v0
-                """
-        )
+        arrayOf(
+            backgroundPlaybackManagerFingerprint to "isBackgroundPlaybackAllowed",
+            backgroundPlaybackManagerShortsFingerprint to "isBackgroundShortsPlaybackAllowed",
+        ).forEach { (fingerprint, extensionsMethod) ->
+            fingerprint.mutableMethodOrThrow().apply {
+                findInstructionIndicesReversedOrThrow(Opcode.RETURN).forEach { index ->
+                    val register = getInstruction<OneRegisterInstruction>(index).registerA
 
-        backgroundPlaybackResumeFingerprint.mutableMethodOrThrow().addInstructions(
-            0, """
-                invoke-static {}, $PLAYER_CLASS_DESCRIPTOR->getBackgroundPlaybackResumeState()Z
-                move-result v0
-                return v0
-                """
-        )
-
-        // endregion
-
-        // region patch for background playback for kids videos
-
-        kidsBackgroundFingerprint.mutableMethodOrThrow().addInstructions(
-            0, """
-                invoke-static {}, $PLAYER_CLASS_DESCRIPTOR->getBackgroundPlaybackState()Z
-                move-result v0
-                return v0
-                """
-        )
-
-        // endregion
-
-        // region patch for exclusive audio playback
-
-        val videoStageMatch = videoStageFingerprint.matchOrThrow()
-        val videoStageMethod = videoStageMatch.method
-        val videoStageClassDef = videoStageMatch.classDef
-        val videoStageMutableMethod = mutableClassDefBy(videoStageClassDef.type).methods.first {
-            MethodUtil.methodSignaturesMatch(it, videoStageMethod)
-        }
-        videoStageMutableMethod.apply {
-            val startIndex = videoStageMatch.patternMatch!!.startIndex
-            val targetIndex = indexOfFirstInstructionOrThrow(startIndex, Opcode.INVOKE_VIRTUAL)
-            val register = getInstruction<FiveRegisterInstruction>(targetIndex).registerC
-
-            addInstructions(
-                targetIndex, """
-                        invoke-static {v$register}, $PLAYER_CLASS_DESCRIPTOR->checkAudioOnlyState(Ljava/lang/Object;)Ljava/lang/Object;
-                        move-result-object v$register
+                    addInstructionsAtControlFlowLabel(
+                        index,
                         """
+                            invoke-static { v$register }, $EXTENSION_CLASS_DESCRIPTOR->$extensionsMethod(Z)Z
+                            move-result v$register 
+                        """,
+                    )
+                }
+            }
+        }
+
+        // Enable background playback option in YouTube settings
+        backgroundPlaybackSettingsFingerprint.matchOrThrow().let { match ->
+            val method = match.method
+            val classDef = match.classDef
+            val mutableMethod = mutableClassDefBy(classDef.type).methods.first {
+                MethodUtil.methodSignaturesMatch(it, method)
+            }
+            val booleanCalls = mutableMethod.instructions.withIndex().filter {
+                it.value.getReference<MethodReference>()?.returnType == "Z"
+            }
+            val settingsBooleanIndex = booleanCalls.elementAt(1).index
+            // Navigate to that instruction to get the method called
+            val invokeInstruction = mutableMethod.getInstruction<com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction>(settingsBooleanIndex)
+            val methodRef = invokeInstruction.reference as? MethodReference
+                ?: throw PatchException("No method reference at index $settingsBooleanIndex")
+            val settingsBooleanMethod = mutableClassDefBy(methodRef.definingClass).methods.first {
+                it.name == methodRef.name && it.parameterTypes == methodRef.parameterTypes
+            }
+            settingsBooleanMethod.returnEarly(true)
+        }
+
+        // Force allowing background play for Shorts.
+        shortsBackgroundPlaybackFeatureFlagFingerprint.injectLiteralInstructionBooleanCall(
+            SHORTS_BACKGROUND_PLAYBACK_FEATURE_FLAG,
+            "$EXTENSION_CLASS_DESCRIPTOR->isBackgroundShortsPlaybackAllowed(Z)Z"
+        )
+
+        // Fix PiP mode issue.
+        if (is_19_34_or_greater) {
+            arrayOf(
+                backgroundPlaybackManagerCairoFragmentPrimaryFingerprint,
+                backgroundPlaybackManagerCairoFragmentSecondaryFingerprint
+            ).forEach { fingerprint ->
+                fingerprint.matchOrThrow(backgroundPlaybackManagerCairoFragmentParentFingerprint)
+                    .let { match ->
+                        val method = match.method
+                        val classDef = match.classDef
+                        val mutableMethod = mutableClassDefBy(classDef.type).methods.first {
+                            MethodUtil.methodSignaturesMatch(it, method)
+                        }
+                        mutableMethod.apply {
+                            val insertIndex = match.patternMatch!!.startIndex + 4
+                            val insertRegister =
+                                getInstruction<OneRegisterInstruction>(insertIndex).registerA
+
+                            addInstruction(
+                                insertIndex,
+                                "const/4 v$insertRegister, 0x0"
+                            )
+                        }
+                    }
+            }
+
+            pipInputConsumerFeatureFlagFingerprint.injectLiteralInstructionBooleanCall(
+                PIP_INPUT_CONSUMER_FEATURE_FLAG,
+                "0x0"
             )
         }
 
-        val audioTrackCheckMatch = audioTrackCheckFingerprint.matchOrThrow()
-        val audioTrackCheckMethod = audioTrackCheckMatch.method
-        val audioTrackCheckClassDef = audioTrackCheckMatch.classDef
-        val audioTrackCheckMutableMethod = mutableClassDefBy(audioTrackCheckClassDef.type).methods.first {
-            MethodUtil.methodSignaturesMatch(it, audioTrackCheckMethod)
-        }
-        audioTrackCheckMutableMethod.apply {
-            val startIndex = audioTrackCheckMatch.patternMatch!!.startIndex
-            val targetIndex = indexOfFirstInstructionOrThrow(startIndex, Opcode.IGET)
-            val register = getInstruction<TwoRegisterInstruction>(targetIndex).registerA
-
-            addInstructions(
-                targetIndex, """
-                        invoke-static {v$register}, $PLAYER_CLASS_DESCRIPTOR->getAudioOnlyState(Z)Z
-                        move-result v$register
-                        """
-            )
-        }
-
-        // endregion
-
-        kidsBackgroundPlaybackPolicyControllerFingerprint.mutableMethodOrThrow().addInstruction(
-            0, "return-void"
-        )
+        // Force allowing background play for videos labeled for kids.
+        kidsBackgroundPlaybackPolicyControllerFingerprint.mutableMethodOrThrow(
+            kidsBackgroundPlaybackPolicyControllerParentFingerprint
+        ).returnEarly()
 
         // region add settings
         addPreference(
             arrayOf(
-                "PREFERENCE_SCREEN: GENERAL",
-                "SETTINGS: REMOVE_BACKGROUND_PLAYBACK_RESTRICTIONS"
+                "PREFERENCE_SCREEN: SHORTS",
+                "SETTINGS: DISABLE_SHORTS_BACKGROUND_PLAYBACK"
             ),
             REMOVE_BACKGROUND_PLAYBACK_RESTRICTIONS
         )
