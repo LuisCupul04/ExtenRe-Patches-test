f76743ed9757a604cf0d88068249c3e6bd1d40c8
Luis Cupul 04 <256816875+LuisCupul04@users.noreply.github.com>
2026-03-22 17:37:34 -0600

    Update build.gradle.kts

--- extensions/shared/build.gradle.kts
+++ extensions/shared/build.gradle.kts
@@ -112,3 +112,8 @@ tasks.register<Sync>("syncExtension") {
     from(dexOutputDir) { include(fileName) }
     into(dexOutputDir.parentFile)
 }
+
+afterEvaluate {
+    tasks.findByName("generateExtensionDex")?.enabled = false
+}
+
