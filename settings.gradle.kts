e5e0fc5a7eb888e9fd7a9454a205db0e760448b7
Luis Cupul 04 <256816875+LuisCupul04@users.noreply.github.com>
2026-03-23 03:55:13 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -41,7 +41,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.extenre.patches") version "1.0.7.dev-RE"
+    id("com.extenre.patches") version "1.0.8.dev-RE"
 }
 
 rootProject.name = "extenre-patches"
