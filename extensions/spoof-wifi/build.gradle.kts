43ff15ea07dd0e4d0a876a41fc424a11438867fe
Luis Cupul 04 <256816875+LuisCupul04@users.noreply.github.com>
2026-03-22 19:22:38 -0600

    Update build.gradle.kts

--- extensions/spoof-wifi/build.gradle.kts
+++ extensions/spoof-wifi/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     id("com.android.application")
 }
 
-val extensionName = "extensions/all/connectivity/wifi/spoof/spoof-wifi.re"
+extension {
+    name = "extensions/all/connectivity/wifi/spoof/spoof-wifi.re"
+}
 
 android {
     namespace = "com.extenre.extension"
@@ -31,43 +33,3 @@ dependencies {
     compileOnly(libs.annotation)
     coreLibraryDesugaring(libs.desugar.jdk.libs)
 }
-
-val parentPath = extensionName.substringBeforeLast('/')
-val fileName = extensionName.substringAfterLast('/')
-
-tasks.register<Sync>("syncExtension") {
-    dependsOn("assembleRelease")
-
-    val apkFile = layout.buildDirectory.file("outputs/apk/release/${project.name}-release.apk").get().asFile
-    val extractDir = layout.buildDirectory.dir("tmp/extractApk").get().asFile
-    val dexOutputDir = layout.buildDirectory.dir("extenre/$parentPath").get().asFile
-
-    doFirst {
-        extractDir.deleteRecursively()
-        extractDir.mkdirs()
-        copy {
-            from(zipTree(apkFile))
-            into(extractDir)
-        }
-
-        val classesDex = extractDir.resolve("classes.dex")
-        if (!classesDex.exists()) {
-            throw GradleException("classes.dex not found in APK: $apkFile")
-        }
-
-        dexOutputDir.mkdirs()
-        copy {
-            from(classesDex)
-            into(dexOutputDir)
-            rename { fileName }
-        }
-    }
-
-    from(dexOutputDir) { include(fileName) }
-    into(dexOutputDir.parentFile)
-}
-
-afterEvaluate {
-    tasks.findByName("generateExtensionDex")?.enabled = false
-}
-
